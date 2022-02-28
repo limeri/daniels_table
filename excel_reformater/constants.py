e79db3bd7bc16c177ad1f66d0930c7540c0848d9
unknown <andylimeripub@gmail.com>
2022-02-27 23:11:04 -0500

    Complete POC to create spreadsheet that can be imported into LGL

---
+++ excel_reformater/constants.py
@@ -0,0 +1,44 @@
+# This file contains constants useful for the code.  Most of these constants are field names to ensure
+# they are never spelled incorrectly.
+
+IGNORE_FIELD = '***** IGNORE THIS FIELD *****'
+
+# LGL Fields
+LGL_CONSTITUENT_ID = 'LGL Constituent ID'
+LGL_FULL_NAME = 'Full Name'
+LGL_FIRST_NAME = 'First name'
+LGL_LAST_NAME = 'Last name'
+LGL_EMAIL_ADDRESS = 'Email address'
+LGL_ADDRESS_LINE_1 = 'Address line 1'
+LGL_ADDRESS_LINE_2 = 'Address line 2'
+LGL_ADDRESS_LINE_3 = 'Address line 3'
+LGL_CITY = 'City'
+LGL_COUNTRY = 'Country'
+LGL_STATE = 'State/province'
+LGL_POSTAL_CODE = 'Postal/ZIP code'
+LGL_COMPANY = 'Employer/Organization'
+LGL_GIFT_DATE = 'Gift date'
+LGL_GIFT_AMOUNT = 'Gift amount'
+LGL_GIFT_NOTE = 'Gift note'
+LGL_EXTERNAL_GIFT_ID = 'External gift ID'
+
+# Fidelity input fields
+FID_ACH_GROUP_ID = "ACH Group Id"
+FID_ACKNOWLEDGEMENT_ADDRESS_LINE_1 = "Acknowledgement Address Line 1"
+FID_ACKNOWLEDGEMENT_ADDRESS_LINE_2 = "Acknowledgement Address Line 2"
+FID_ACKNOWLEDGEMENT_ADDRESS_LINE_3 = "Acknowledgement Address Line 3"
+FID_ACKNOWLEDGEMENT_CITY = "Acknowledgement City"
+FID_ACKNOWLEDGEMENT_COUNTRY = "Acknowledgement Country"
+FID_ACKNOWLEDGEMENT_STATE = "Acknowledgement State"
+FID_ACKNOWLEDGEMENT_ZIPCODE = "Acknowledgement ZipCode"
+FID_ADDRESSEE_NAME = "Addressee Name"
+FID_EFFECTIVE_DATE = "Effective Date"
+FID_FULL_ADDRESS = "Full Address"
+FID_GIVING_ACCOUNT_NAME = "Giving Account Name"
+FID_GRANT_AMOUNT = "Grant Amount"
+FID_GRANT_ID = "Grant Id"
+FID_PAYABLE_TO = "Payable To"
+FID_PRIMARY_NAME = "Primary Name"
+FID_RECOMMENDED_BY = "Recommended By"
+FID_SECONDARY_NAME = "Secondary Name"
+FID_SPECIAL_PURPOSE = "Special Purpose"
--- excel_reformater/excel_elt.py
+++ excel_reformater/excel_elt.py
@@ -1,12 +1,109 @@
+# https://pandas.pydata.org/pandas-docs/stable/user_guide/dsintro.html
+
+import constants as cc
 import os
 import pandas as pd
-# os.chdir('c:\\dev\\daniels_table\\excel_reformater')
 
 SAMPLE_FILE = '2022fidelity.xlsx'
 
-abs_script_path = os.path.abspath(__file__)
-working_dir = os.path.dirname(abs_script_path)
-os.chdir(working_dir)
 
-df = pd.read_excel(SAMPLE_FILE)
-print(df['Giving Account Name'][2])
+class ExcelFileReader:
+    NONE = 'None'
+
+    FIDELITY_MAP = {
+        cc.FID_ADDRESSEE_NAME: cc.LGL_FULL_NAME,
+        cc.FID_ACKNOWLEDGEMENT_ADDRESS_LINE_1: cc.LGL_ADDRESS_LINE_1,
+        cc.FID_ACKNOWLEDGEMENT_ADDRESS_LINE_2: cc.LGL_ADDRESS_LINE_2,
+        cc.FID_ACKNOWLEDGEMENT_ADDRESS_LINE_3: cc.LGL_ADDRESS_LINE_3,
+        cc.FID_ACKNOWLEDGEMENT_CITY: cc.LGL_CITY,
+        cc.FID_ACKNOWLEDGEMENT_COUNTRY: cc.LGL_COUNTRY,
+        cc.FID_ACKNOWLEDGEMENT_STATE: cc.LGL_STATE,
+        cc.FID_ACKNOWLEDGEMENT_ZIPCODE: cc.LGL_POSTAL_CODE,
+        cc.FID_GRANT_ID: cc.LGL_EXTERNAL_GIFT_ID,
+        cc.FID_EFFECTIVE_DATE: cc.LGL_GIFT_DATE,
+        cc.FID_GRANT_AMOUNT: cc.LGL_GIFT_AMOUNT,
+        cc.FID_SPECIAL_PURPOSE: cc.LGL_GIFT_NOTE,
+        # --- These fields are ignored --- #
+        cc.FID_RECOMMENDED_BY: cc.IGNORE_FIELD,
+        cc.FID_ACH_GROUP_ID: cc.IGNORE_FIELD,
+        cc.FID_FULL_ADDRESS: cc.IGNORE_FIELD,
+        cc.FID_GIVING_ACCOUNT_NAME: cc.IGNORE_FIELD,
+        cc.FID_PAYABLE_TO: cc.IGNORE_FIELD,
+        cc.FID_PRIMARY_NAME: cc.IGNORE_FIELD,
+        cc.FID_SECONDARY_NAME: cc.IGNORE_FIELD,
+    }
+
+    # This method reads the data from an Excel spreadsheet and returns a datafile.  This
+    # method requires the Pandas module to be installed.
+    #
+    # Args:
+    #   file_path = path to the Excel file being read
+    #
+    # Returns - a Pandas data frame containing the data
+    @staticmethod
+    def read_file(file_path):
+        df = pd.read_excel(file_path)
+        return df
+
+    # This method will map fields based on the dictionary map specified.
+    # To do this bit of magic, the input data frame will be converted to a Python dict and a new Python dict with
+    # the correct labels and data will be created.  The new Python dict can then be converted back to a data frame
+    # for output.  The new data frame will be converted into the output Excel file.
+    #
+    # The format of the input dict (from the input_df) is:
+    #   {column_name_1 {0: <row data>, 1: <row data>, ...}, column_name_2 ...}
+    #
+    # Some sample data:
+    # {'Recommended By': {0: 'Online at FC', 1: 'Online at FC', 2: 'Online at FC'},
+    #  'Grant Id': {0: 17309716, 1: 17319469, 2: 17401868},
+    #
+    # The goal is to modify the names of the outer keys.  In the sample data above, "Recommended By" is ignored
+    # (it is not included in the final output) and "Grant Id" is changed to "External gift ID".  The inner dict
+    # (with keys 0, 1, ...) is unchanged.
+    #
+    # Args:
+    #   input_df = a Pandas data frame from an Excel or CSV file
+    #   map = a dict mapping column headers from input_df to an output data frame
+    #
+    # Returns - a Pandas data frame containing the converted data.
+    def map_fields(self, input_df, field_map):
+        input_data = input_df.to_dict()
+        input_keys = input_data.keys()
+        output_data = {}
+        for input_key in input_keys:
+            if input_key not in field_map.keys():
+                print('The key "{}" was not found.'.format(input_key))
+            output_key = field_map[input_key]
+            if output_key == cc.IGNORE_FIELD:
+                print('Ignoring key "{}".'.format(input_key))
+                continue
+            output_data[output_key] = input_data[input_key]
+#        return output_data
+        output_df = pd.DataFrame(output_data)
+        return output_df
+
+
+def run_sample_test():
+    abs_script_path = os.path.abspath(__file__)
+    working_dir = os.path.dirname(abs_script_path)
+    os.chdir(working_dir)
+    excell = ExcelFileReader()
+    df = excell.read_file(file_path=SAMPLE_FILE)
+    print('-----')
+    print(df.to_string() + "\n\n")
+    print('-----\nAccount Name: ', df['Giving Account Name'].to_string())
+    print('-----\nAccount Name, Row 2: ', df['Giving Account Name'][2])
+    print('-----\nIterate over rows:')
+    for index, row in df.iterrows():
+        print(index, ": ", row.to_string())
+
+    # input_data = df.to_dict()
+    # print('----- input dict conversion\n', input_data)
+
+    output = excell.map_fields(df, field_map=excell.FIDELITY_MAP)
+    print('----- output dict conversion\n', output.to_string())
+    output_file = open('lgl.csv', 'w')
+    output_file.write(output.to_csv(index=False, line_terminator='\n'))
+
+if __name__ == '__main__':
+    run_sample_test()
